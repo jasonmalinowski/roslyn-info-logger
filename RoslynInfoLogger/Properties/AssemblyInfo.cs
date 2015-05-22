973bdb6221ca0f47ea108ebfe6ca81aad063d2f4
Jason Malinowski <jason.malinowski@microsoft.com>
2015-05-21 21:05:59 -0700

    Initial commit of empty VSIX project

---
+++ RoslynInfoLogger/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Roslyn Info Logger")]
+
+[assembly: AssemblyVersion("0.1")]
+[assembly: AssemblyFileVersion("0.1")]
